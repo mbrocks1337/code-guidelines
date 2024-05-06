ad4564d5937f93f039ca9c6a147f4826e277a8dc
Marc Brocks <m.brocks@mediahaus.de>
2024-05-06 15:13:43 +0200

    fix: fix color in dark mode

--- components/ColorSwitcher.vue
+++ components/ColorSwitcher.vue
@@ -9,20 +9,20 @@ const colorMode = useColorMode();
     <DropdownMenuTrigger as-child>
       <Button variant="secondary" size="icon" class="rounded-full">
         <Sun
-          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100dark:-rotate-90 dark:scale-0"
         />
         <Moon
-          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100"
         />
         <span class="sr-only">Toggle theme</span>
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent align="end">
       <DropdownMenuItem @click="colorMode.preference = 'light'">
-        Light
+        Hell
       </DropdownMenuItem>
       <DropdownMenuItem @click="colorMode.preference = 'dark'">
-        Dark
+        Dunkel
       </DropdownMenuItem>
       <DropdownMenuItem @click="colorMode.preference = 'system'">
         System
