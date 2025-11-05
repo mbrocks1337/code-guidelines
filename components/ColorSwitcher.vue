94410ce8dd8cdbd44ba2b5b7b4dc4a95235ef6a8
Marc Brocks <m.brocks@mediahaus.de>
2025-11-05 15:06:14 +0100

    add first few tasks

--- components/ColorSwitcher.vue
+++ components/ColorSwitcher.vue
@@ -16,7 +16,7 @@ function changeColorMode() {
     @click="changeColorMode"
   >
     <Sun
-      class="scale-100dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 dark:scale-0"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 dark:-rotate-90 dark:scale-0"
     />
     <Moon
       class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 dark:rotate-0 dark:scale-100"
--- pages/tasks/level-3.vue
+++ pages/tasks/level-3.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import type { LayoutItem } from "~/components/App/AppLayout.vue";
+
+const layout: LayoutItem[] = [];
 </script>
 <template>
   <AppLayout title="Übungsaufgaben (Schwer)" :layout />
